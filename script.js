2a6479ddd5fe19dcb41f5bdf22c2f24def104ee3
Tim Loughrist <tim.loughrist@gmail.com>
2022-07-04 07:19:06 -0500

    nearly done

--- script.js
+++ script.js
@@ -1,4 +1,80 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const td1a = document.getElementById('td1a');
+td1a.addEventListener('mouseenter', function() {
+  td1a.style.backgroundColor = "red";
+});
 
-document.querySelector("body").appendChild(h2);
+td1a.addEventListener('mouseleave', function() {
+  td1a.style.backgroundColor = "white";
+});
+
+const td1b = document.getElementById('td1b');
+td1b.addEventListener('mouseenter', function() {
+  td1b.style.backgroundColor = "orange";
+});
+
+td1b.addEventListener('mouseleave', function() {
+  td1b.style.backgroundColor = "white";
+});
+
+const td1c = document.getElementById('td1c');
+td1c.addEventListener('mouseenter', function() {
+  td1c.style.backgroundColor = "yellow";
+});
+
+td1c.addEventListener('mouseleave', function() {
+  td1c.style.backgroundColor = "white";
+});
+
+const td2a = document.getElementById('td2a');
+td2a.addEventListener('mouseenter', function() {
+  td2a.style.backgroundColor = "green";
+});
+
+td2a.addEventListener('mouseleave', function() {
+  td2a.style.backgroundColor = "white";
+});
+
+const td2b = document.getElementById('td2b');
+td2b.addEventListener('mouseenter', function() {
+  td2b.style.backgroundColor = "#00A86B";
+});
+
+td2b.addEventListener('mouseleave', function() {
+  td2b.style.backgroundColor = "white";
+});
+
+const td2c = document.getElementById('td2c');
+td2c.addEventListener('mouseenter', function() {
+  td2c.style.backgroundColor = "#008B8B";
+});
+
+td2c.addEventListener('mouseleave', function() {
+  td2c.style.backgroundColor = "white";
+});
+
+const td3a = document.getElementById('td3a');
+td3a.addEventListener('mouseenter', function() {
+  td3a.style.backgroundColor = "blue";
+});
+
+td3a.addEventListener('mouseleave', function() {
+  td3a.style.backgroundColor = "white";
+});
+
+const td3b = document.getElementById('td3b');
+td3b.addEventListener('mouseenter', function() {
+  td3b.style.backgroundColor = "indigo";
+});
+
+td3b.addEventListener('mouseleave', function() {
+  td3b.style.backgroundColor = "white";
+});
+
+const td3c = document.getElementById('td3c');
+td3c.addEventListener('mouseenter', function() {
+  td3c.style.backgroundColor = "violet";
+});
+
+td3c.addEventListener('mouseleave', function() {
+  td3c.style.backgroundColor = "white";
+});
